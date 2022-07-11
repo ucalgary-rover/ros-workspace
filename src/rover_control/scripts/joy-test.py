2624d7df4565ec4a01231a34012cc762ae1a55a2
CarterM2607 <cartermar211@gmail.com>
2022-07-11 16:37:05 -0600

    changes to joy-test.py

--- src/rover_control/scripts/joy-test.py
+++ src/rover_control/scripts/joy-test.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python 
 from geometry_msgs.msg import Twist
+import std_msgs
 import time
 import rospy
 
 def callback(data):
-    rospy.loginfo("linear x = %d",data.linear.x)
+	rospy.sleep(0.25)
+	linear_v = round(data.linear.x,1)
+	angular_v = -1*round(data.angular.z,1)
+	a =[linear_v,angular_v]
+	rospy.loginfo(a)
 def main():
 	rospy.init_node('test_inputs', anonymous=True)
-	rospy.Subscriber("rover/cmd_vel", Twist, callback)
+	rospy.Subscriber("rover/cmd_vel", Twist, callback,queue_size=3)
+	
 	rospy.spin()
-
 if __name__ == '__main__':
 	try:
 		main()
